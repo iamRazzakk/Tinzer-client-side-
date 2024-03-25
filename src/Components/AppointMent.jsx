22d4987d697803c9f64ad75ebbae48a85e566a2d
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2024-03-25 11:54:48 +0600

    create appointment form

--- src/Components/AppointMent.jsx
+++ src/Components/AppointMent.jsx
@@ -3,21 +3,39 @@
 const AppointMent = () => {
     return (
         <div className="text-primary">
-            <form className="card-body lg:px-28 lg:space-y-6 min-h-full">
-                < div className="form-control" >
+            <div className="text-primary">
+                <form className="card-body min-h-screen rounded-t-lg bg-secondary lg:px-28 lg:space-y-6 lg:w-2/3 lg:mx-auto">
+                    <h1 className="text-center text-3xl font-bold text-white">Book Your Appointment</h1>
+                    < div className="form-control" >
 
-                    <input type="text" name="name" placeholder="Enter your Name" className="input input-bordered bg-white " required />
-                </div >
-                < div className="form-control" >
+                        <input type="text" name="name" placeholder="Enter your Name" className="input input-bordered bg-white " required />
+                    </div >
+                    < div className="form-control" >
 
-                    <input type="email" name="email" placeholder="Enter your email" className="input input-bordered bg-white " required />
-                </div >
-                <div className="form-control">
-
-                    <input type="password" name="password" placeholder="Enter your password" className="input input-bordered bg-white " required />
-                </div>
-                
-            </form >
+                        <input type="email" name="email" placeholder="Enter your email" className="input input-bordered bg-white " required />
+                    </div >
+                    <div className="form-control">
+                        <input type="number" name="phone" placeholder="Enter your Phone Number" className="input input-bordered bg-white " required />
+                    </div>
+                    <div className="form-control">
+                        <select className="p-3 rounded-lg bg-white text-primary border border-white" name="cars" id="cars">
+                            <option value="volvo">Select Doctor</option>
+                            <option value="Myles">Dr Myles</option>
+                            <option value="Abbed">Dr Khalid Abbed</option>
+                            <option value="Trehan">Dr Naresh Trehan</option>
+                            <option value="Abright">Dr Arthur Reese Abright</option>
+                        </select>
+                    </div>
+                    {/* button */}
+                    <a className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-white rounded-full shadow-md group">
+                        <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-primary group-hover:translate-x-0 ease">
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                        </span>
+                        <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Submit</span>
+                        <span className="relative invisible">Submit</span>
+                    </a>
+                </form >
+            </div>
         </div>
     );
 };
--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -32,23 +32,21 @@ const Dashboard = () => {
     return (
         <div className="text-white font-Poppins flex md:mt-10 max-w-3xl lg:max-w-screen-xl">
             <div className="w-40 md:w-64 min-h-full bg-primary">
-                {/* Your menu items */}
 
                 <ul className="menu md:p-6  md:text-xl">
                     {/* Show admin dashboard if current user is admin */}
                     {currentUser?.role === "Admin" &&
                         <>
                             <li className="md:mb-4"><NavLink to='/dashboard/profile'>Admin Profile</NavLink></li>
-                            <li className="md:mb-4"><NavLink to='/dashboard/admin-dashboard'>Admin Dashboard</NavLink></li>
+                            <li className="md:mb-4"><NavLink to='/dashboard/history'>Appointment History</NavLink></li>
                             <li className="md:mb-4"><NavLink to='/dashboard/users'>All User</NavLink></li>
-                            {/* Add other admin-specific menu items */}
                         </>
                     }
                     {/* Show user dashboard if current user is not admin */}
                     {currentUser?.role === "user" &&
                         <>
                             <li className="md:mb-4"><NavLink to='/dashboard/profile'>User Profile</NavLink></li>
-                            
+                            <li className="md:mb-4"><NavLink to='/dashboard/my-appointment'>My Appointment</NavLink></li>
                             {/* Add other user-specific menu items */}
                         </>
                     }
