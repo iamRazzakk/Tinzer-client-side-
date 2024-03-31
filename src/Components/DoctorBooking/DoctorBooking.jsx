ca8687bb38bd6694c82612e2fc5cb02c28b4e99e
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2024-03-31 21:51:27 +0600

    nothing is commit

--- src/Components/DoctorBooking/DoctorBooking.jsx
+++ src/Components/DoctorBooking/DoctorBooking.jsx
@@ -1,15 +1,34 @@
+import { useContext } from "react";
+import { AuthContext } from './../Shared/AuthProvider';
+import { useQuery } from "@tanstack/react-query";
 
 const DoctorBooking = () => {
+    const { user } = useContext(AuthContext)
+    const { data: users } = useQuery({
+        queryKey: 'users',
+        queryFn: () => {
+            return fetch("http://localhost:5000/users")
+                .then((res) => res.json());
+        },
+    });
+    const currentUser = users?.find(u => u.email === user?.email);
     return (
-        <div className="lg:mt-10 mt-4 flex">
-            <div className="flex-1">
-                Chat application
+        <div className="lg:mt-10 mt-4 lg:flex items-center">
+            <div className="flex-1 ">
+                <h1>
+                    Patient Name: {currentUser?.name}
+                </h1>
+                <h1>
+                    Payment method("Work on it")
+                </h1>
             </div>
-            <div className="flex-1 bg-primary flex justify-center  text-white">
-                
+            <div className="flex-1">
+                {/* <img src="" alt="" />
+                <h2>Doctor Name: {specialities?.doctor_name}</h2> */}
             </div>
         </div>
     );
 };
 
 export default DoctorBooking;
+// 
--- src/Components/SpecialitiesDoctors/Physician.jsx
+++ src/Components/SpecialitiesDoctors/Physician.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { MdLaptopChromebook } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { FaVideo } from "react-icons/fa6";
 
 const Physician = () => {
     const { data: specialities } = useQuery({
@@ -26,16 +26,22 @@ const Physician = () => {
                 <div className="lg:mt-10 text-white">
                     <a className="flex flex-col items-center bg-white border border-primary rounded-lg shadow md:flex-row   dark:border-secondary dark:bg-primary lg:h-[194px] w-full  ">
                         <img className="flex-1 w-20 p-10  h-full object-cover" src={physician?.image} alt="" />
-                        <div className="flex  flex-col justify-between p-4 leading-normal">
+                        <div className="flex flex-1  flex-col justify-between p-4 leading-normal">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{physician?.doctor_name}</h5>
                             <p className="mb-3 font-normal dark:text-white">{physician?.title}</p>
                             <p className="mb-3 font-normal dark:text-white">{physician?.specialist}</p>
                         </div>
                         <div className="flex-1">
-                            <p className="mb-3 font-normal dark:text-white">Price: {physician?.price}</p>
-                            <Link to="/book-now" className="tooltip lg:mt-10 tooltip-open tooltip-accent" data-tip="Book now">
-                                <MdLaptopChromebook className="text-2xl" />
-                            </Link>
+                            <p className="mb-3 font-normal dark:text-white">Consultation fee: {physician?.price}</p>
+
+                            <div className="lg:mt-10 ">
+                                <Link to="/book-now">
+                                    <button className="flex items-center gap-3 bg-secondary px-3 py-4 rounded-lg">
+                                        <FaVideo></FaVideo>
+                                        <h1>See Doctor Now</h1>
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
                     </a>
                 </div>
